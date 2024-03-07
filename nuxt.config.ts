51f1087749f35a3f39a4af48eeed0a5ccd079583
rali11 <edu_2015@live.com>
2024-03-07 17:06:32 -0300

    feat(components): add Hero.vue section

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
+  modules: ['@nuxtjs/google-fonts'],
+  googleFonts:{
+    families:{
+      Roboto: {
+        wght:[300,400,700]
+      }
+    }
+  },
   vite: {
     css: {
       preprocessorOptions: {
