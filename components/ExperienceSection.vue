7e7dcedaba67167c93d887d0b54659316274cd1c
rali11 <edu_2015@live.com>
2024-03-23 20:39:45 -0300

    refactor(experience): ref text in experiencie

--- components/ExperienceSection.vue
+++ components/ExperienceSection.vue
@@ -22,8 +22,8 @@
       title:'Desarrollador Full-Stack',
       company:'Policia Federal Argentina',
       descripcion:`Lider y desarrollador de equipo. 
-       Responsable del mantenimiento y mejora continua de varias aplicaciones web críticas, 
-       algunas de ellas se encargan de la gestión de moviles policiales, y administración de credenciales. 
+       Responsable del mantenimiento y mejora continua de varias aplicaciones web críticas. 
+       Algunas de las cuales incluyen funciones importantes como la gestión de moviles policiales, y administración de credenciales. 
        Lenguajes utilizados: Javascript (Vue.js), PHP (Symfony) y PL-SQL (Oracle DB). 
       `,
       date:'2016 - Actualmente...'
