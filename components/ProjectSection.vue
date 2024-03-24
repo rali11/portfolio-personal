567fb74d60a35518b89f5d068f1e277b288af3f0
rali11 <edu_2015@live.com>
2024-03-24 20:24:51 -0300

    feat(ui): add badge & icons components

--- components/ProjectSection.vue
+++ components/ProjectSection.vue
@@ -13,7 +13,21 @@
         :img-url="project.imgUrl"
         :code-url="project.codeUrl"
         :preview-url="project.previewUrl"
-      />
+      >
+        <Badge 
+          v-for="(badge, badgeIndex) in project.badges"
+          :key="badgeIndex"
+          :text-color="badge.textColor"
+          :background-color="badge.backgroundColor"
+        >
+          <Component 
+            :is="resolveComponent(badge.icon)"
+            width="1rem"
+            height="1rem" 
+          />
+          {{ badge.text }}
+        </Badge>
+      </ProjectItem>
     </div>
   </Container>
 </template>
@@ -26,14 +40,21 @@
       description: 'Quiz sobre geografia. Preguntas para adivinar la capital de un país o a que país pertenece una bandera. Desarrollado con Vue y Sass.',
       imgUrl:'/projects/project-quiz.webp',
       codeUrl:'https://github.com/rali11/country-quiz-2',
-      previewUrl:'https://musical-meringue-211f04.netlify.app/'
+      previewUrl:'https://musical-meringue-211f04.netlify.app/',
+      badges:[
+        { textColor:'#42B883', backgroundColor:'#2f2f2f', text:'Vue.js',icon:'VueIcon'},
+        { textColor:'#bf4080', backgroundColor:'#2f2f2f', text:'Sass',icon:'SassIcon'},
+      ]
     },
     {
       title: 'Weather App - Información del Clima',
       description: 'Es una aplicación de pronóstico del tiempo que proporciona información en tiempo real y pronósticos para los próximos 5 días. Incluye datos sobre la humedad, visibilidad, presión del aire y velocidad del viento. Desarrollado con Vue.',
       imgUrl:'/projects/project-weather.webp',
       codeUrl:'https://github.com/rali11/weather-app',
-      previewUrl:'https://admirable-trifle-5afe73.netlify.app'
+      previewUrl:'https://admirable-trifle-5afe73.netlify.app',
+      badges:[
+        { textColor:'#42B883', backgroundColor:'#2f2f2f', text:'Vue.js',icon:'VueIcon'},
+      ]
     }
   ])
 
---
+++ components/ui/atoms/Badge.vue
@@ -0,0 +1,35 @@
+<template>
+  <span class="badge">
+    <slot />
+  </span>
+</template>
+
+<script lang="ts" setup>
+  interface Props {
+    textColor: string
+    backgroundColor: string
+  }
+
+  const props = withDefaults(defineProps<Props>(), {
+    textColor:'#000',
+    backgroundColor:'#fff'
+  })
+
+  const textColor = ref(props.textColor)
+  const backgroundColor = ref(props.backgroundColor)
+</script>
+
+<style lang="scss" scoped>
+  .badge {
+    color : v-bind(textColor);
+    background-color: v-bind(backgroundColor);
+    border-radius: 15px;
+    padding: 4px 10px;
+    display: flex;
+    align-items: center;
+    width: max-content;
+    gap: 5px;
+    font-size: .9rem;  
+    margin: 0;
+  }
+</style>
